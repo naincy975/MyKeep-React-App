8934064d4cada16833b28275a1da64de972899a5
naincy975 <naincyrastogi975@gmail.com>
2023-02-17 22:53:21 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
